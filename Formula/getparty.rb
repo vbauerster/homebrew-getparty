8abaf3c8cca1b1bf5ad46eadda2465c745b49380
vbauerster <vbauerster@gmail.com>
2019-04-18 01:55:47 +0300

    Brew formula update for getparty version v1.6.5

--- Formula/getparty.rb
+++ Formula/getparty.rb
@@ -2,9 +2,9 @@
 class Getparty < Formula
   desc "HTTP Download Manager with multi-parts"
   homepage "https://github.com/vbauerster/getparty"
-  url "https://github.com/vbauerster/getparty/releases/download/v1.6.4/getparty_1.6.4_Darwin_x86_64.tar.gz"
-  version "1.6.4"
-  sha256 "a2e2b27d5a71245c1df4905c89e534db3bc71d8b5fc1cbd422c79c39eda18183"
+  url "https://github.com/vbauerster/getparty/releases/download/v1.6.5/getparty_1.6.5_Darwin_x86_64.tar.gz"
+  version "1.6.5"
+  sha256 "7563b05c9a60e6620b4a20b490836b00c970aedeb9b00b82002958850f5ecb29"
 
   def install
     bin.install "getparty"
