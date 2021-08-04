b186970f6a9e9e44cad9d9ec7770fb14cbd292be
vbauerster <vbauerster@gmail.com>
2021-08-04 17:23:49 +0500

    Brew formula update for getparty version v1.11.3

--- Formula/getparty.rb
+++ Formula/getparty.rb
@@ -5,32 +5,32 @@
 class Getparty < Formula
   desc "HTTP Download Manager with multi-parts"
   homepage "https://github.com/vbauerster/getparty"
-  version "1.11.1"
+  version "1.11.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/vbauerster/getparty/releases/download/v1.11.1/getparty_1.11.1_Darwin_x86_64.tar.gz"
-      sha256 "e57f08593838a4bb10f276be9713260b1ab67f79373704c9b8abc15ccb9eb3ad"
+      url "https://github.com/vbauerster/getparty/releases/download/v1.11.3/getparty_1.11.3_Darwin_x86_64.tar.gz"
+      sha256 "3cf48389ab0de3b6020f395b71b98e355acd6954bb16699dacfc21c5aaea8a9f"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/vbauerster/getparty/releases/download/v1.11.1/getparty_1.11.1_Darwin_arm64.tar.gz"
-      sha256 "1f405d66c480eb5a28e47db3725b3a8a448fdffbfc0a76ab45106151436ce260"
+      url "https://github.com/vbauerster/getparty/releases/download/v1.11.3/getparty_1.11.3_Darwin_arm64.tar.gz"
+      sha256 "0dd59d30735474cf16ea1ee2c11ee4ec36f5da6e2bca6042318240047a351da6"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/vbauerster/getparty/releases/download/v1.11.1/getparty_1.11.1_Linux_x86_64.tar.gz"
-      sha256 "0109a9bbf579b980bb0f189b8fc6c4d056a45c20c1207b6c895d9d5df3c6bfd3"
+      url "https://github.com/vbauerster/getparty/releases/download/v1.11.3/getparty_1.11.3_Linux_x86_64.tar.gz"
+      sha256 "a4194bddc2c7ac54e15be124099c99bb3472020a426b7a2217193f1f34026200"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/vbauerster/getparty/releases/download/v1.11.1/getparty_1.11.1_Linux_armv6.tar.gz"
-      sha256 "192cebe5d1951c1fc07a3ea07c5787565582614e7eceaaa2a15fc11da4f4bfa4"
+      url "https://github.com/vbauerster/getparty/releases/download/v1.11.3/getparty_1.11.3_Linux_armv6.tar.gz"
+      sha256 "4853f7b8de318e4cdeafb64f8eff713360af9f57bc7f988caec3e06ca3e0adac"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vbauerster/getparty/releases/download/v1.11.1/getparty_1.11.1_Linux_arm64.tar.gz"
-      sha256 "cb4dbd26ea71b44577c91c84ea6ade4b3d8f6ca60e88620ee2df47140a84f55e"
+      url "https://github.com/vbauerster/getparty/releases/download/v1.11.3/getparty_1.11.3_Linux_arm64.tar.gz"
+      sha256 "58d999ec219746a185558634b71cb07ce624327d9c7cf4c7cd2359d2fb214ee8"
     end
   end
 
